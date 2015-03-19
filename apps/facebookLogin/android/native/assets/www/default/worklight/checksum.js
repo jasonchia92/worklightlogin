01c1135f690b30636e6618e95a1fac6d1750bc0a
Jason <jasonchia92@gmail.com>
2015-03-19 16:23:45 +0800

    test

---
+++ apps/facebookLogin/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":951870272,"date":1426749999344,"machine":"Neo-Lab"};
+/* Date: Thu Mar 19 15:26:39 CST 2015 */
---
+++ apps/facebookLogin/ipad/native/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":3480494247,"date":1426749998204,"machine":"Neo-Lab"};
+/* Date: Thu Mar 19 15:26:38 CST 2015 */
